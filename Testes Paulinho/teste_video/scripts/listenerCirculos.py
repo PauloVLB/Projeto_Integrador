7b2b4a183e926c7362f4cc424d82b286e483b400
PauloVLB <vitorlimaborges@gmail.com>
2019-04-18 19:44:13 -0300

    Adicionada a publicação das coordenadas dos círculos.

--- Testes Paulinho/teste_video/scripts/listenerCirculos.py
+++ Testes Paulinho/teste_video/scripts/listenerCirculos.py
@@ -5,16 +5,25 @@ import cv2
 import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from std_msgs.msg import Float64MultiArray
 
 def circular(img, circles):
 	if (circles is not None):
 		circles = np.uint16(np.around(circles))
 		for i in circles[0, :]:
-			center = (i[0], i[1])
-			radius = i[2]
+			x, y, r = i[0], i[1], i[2]
+
+			center = (x, y)
+			radius = r
 
 			cv2.circle(img, center, 1, (0,100,100), 3) #centro
 			cv2.circle(img, center, radius, (255, 0, 255), 3) #borda
+
+			arrayCoordenadas.data[0] = x
+			arrayCoordenadas.data[1] = y
+			arrayCoordenadas.data[2] = r
+
+			pub.publish(arrayCoordenadas)
 	return img
 
 def acharCirculos(img):
@@ -25,8 +34,8 @@ def acharCirculos(img):
 	
 	rows = cinza.shape[1]
     
- 	circles = cv2.HoughCircles(cinza, cv2.HOUGH_GRADIENT, 0.1, rows/8, 
- 		param1=100, param2=30, minRadius=20, maxRadius=100)
+ 	circles = cv2.HoughCircles(cinza, cv2.HOUGH_GRADIENT, 1.2, rows/8, 
+ 		param1=100, param2=30, minRadius=10, maxRadius=100)
 
 	copiaImg = circular(copiaImg, circles)
 
@@ -38,10 +47,9 @@ def callback(data):
 
 	img = acharCirculos(imgCV)
 
-	cv2.namedWindow('LISTENER_1', cv2.WINDOW_NORMAL)
+	#cv2.namedWindow('LISTENER_1', cv2.WINDOW_NORMAL)
 	cv2.imshow('LISTENER_1', img)
 	cv2.waitKey(1)
-
 
 def listenerImg():
 	rospy.init_node('listenerCirculo', anonymous=False)
@@ -49,4 +57,9 @@ def listenerImg():
 	rospy.spin()
 
 if __name__ == "__main__":
+	pub = rospy.Publisher('coordenadas_circulos', Float64MultiArray, queue_size=10)
+
+	arrayCoordenadas = Float64MultiArray()
+	arrayCoordenadas.data = [0,0,0]
+	
 	listenerImg()
