6b1107cf44b834a8d5d12db216d6dd56c2fe23c1
PauloVLB <vitorlimaborges@gmail.com>
2019-04-19 23:50:36 -0300

    Testes com criação de .msg

--- Testes Paulinho/teste_video/scripts/listenerCirculos.py
+++ Testes Paulinho/teste_video/scripts/listenerCirculos.py
@@ -6,24 +6,43 @@ import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from std_msgs.msg import Float64MultiArray
+from teste_video.msg import Circulo, Circulos
 
 def circular(img, circles):
+	pt1 = (img.shape[1]/2, img.shape[0])
+	pt2 = (img.shape[1]/2, 0)
+	cv2.line(img, pt1, pt2, (0,255,0), 1)
+
+	pt1 = (img.shape[1], img.shape[0]/2)
+	pt2 = (0, img.shape[0]/2)
+	cv2.line(img, pt1, pt2, (0,255,0), 1)		
+
+
 	if (circles is not None):
 		circles = np.uint16(np.around(circles))
-		for i in circles[0, :]:
-			x, y, r = i[0], i[1], i[2]
+		index = 1
+		for i in circles[0, :]: 
+			x, y, raio = i[0], i[1], i[2]
 
 			center = (x, y)
-			radius = r
 
 			cv2.circle(img, center, 1, (0,100,100), 3) #centro
-			cv2.circle(img, center, radius, (255, 0, 255), 3) #borda
+			cv2.circle(img, center, raio, (255, 0, 255), 3) #borda
 
-			arrayCoordenadas.data[0] = x
-			arrayCoordenadas.data[1] = y
-			arrayCoordenadas.data[2] = r
+			org = (x - raio, y - raio)
 
-			pub.publish(arrayCoordenadas)
+			x = (x+(img.shape[1]/2)*-1)
+			y = ((img.shape[0]/2)-y)
+
+			text = '%s: (%s,%s)'%(str(index), x, y)
+			cv2.putText(img, text, org, cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+			
+			circulo.x = x
+			circulo.y = y
+			circulo.raio = raio
+			circulo.index = index
+			index += 1	
+			pub.publish(circulo)
 	return img
 
 def acharCirculos(img):
@@ -34,7 +53,7 @@ def acharCirculos(img):
 	
 	rows = cinza.shape[1]
     
- 	circles = cv2.HoughCircles(cinza, cv2.HOUGH_GRADIENT, 1.2, rows/8, 
+ 	circles = cv2.HoughCircles(cinza, cv2.HOUGH_GRADIENT, 0.00001, rows/8, 
  		param1=100, param2=30, minRadius=10, maxRadius=100)
 
 	copiaImg = circular(copiaImg, circles)
@@ -57,9 +76,12 @@ def listenerImg():
 	rospy.spin()
 
 if __name__ == "__main__":
-	pub = rospy.Publisher('coordenadas_circulos', Float64MultiArray, queue_size=10)
+	pub = rospy.Publisher('coordenadas_circulos', Circulo, queue_size=10)
+	
+	#arrayCoordenadas = Float64MultiArray()
+	#arrayCoordenadas.data = [0,0,0]
 
-	arrayCoordenadas = Float64MultiArray()
-	arrayCoordenadas.data = [0,0,0]
+	circulo = Circulo()
+	circulos = Circulos()
 	
 	listenerImg()
