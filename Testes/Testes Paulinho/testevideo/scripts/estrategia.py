c72db174b1d4b29525d2159139c1402c57db40f2
Isaac Marlon da Silva Lourenço <isaacmarlondasilvalourenco@gmail.com>
2019-05-19 14:19:36 -0300

    Renomeação de pacote teste_video

--- Testes/Testes Paulinho/testevideo/scripts/estrategia.py
+++ Testes/Testes Paulinho/testevideo/scripts/estrategia.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import rospy
 import message_filters
 from std_msgs.msg import Float64MultiArray
 from std_msgs.msg import Int32MultiArray
-from teste_video.msg import SensoresDistanciaMsg, RefletanciaMsg
-
-
+from std_msgs.msg import Int8MultiArray # 1byte -128 to 127, utilizamos de 0 a 90
+from testevideo.msg import SensoresDistanciaMsg, RefletanciaMsg
 
 def callbackEstrategia(refle, dist):
-    pubMotores = rospy.Publisher('motores', Int32MultiArray, queue_size=10)
+    pubMotores = rospy.Publisher('motores', Int32MultiArray, queue_size=10
     rate = rospy.Rate(20)
 
     maisEsq = refle.refletancia[0]
@@ -17,6 +17,7 @@ def callbackEstrategia(refle, dist):
     dir = refle.refletancia[2]
     maisDir = refle.refletancia[3]
     dataMotores = Int32MultiArray()
+
     if(esq > 4 and dir > 4 ) :
         dataMotores.data = [25, 25]
     elif (esq > 4 and dir < 4  ):
@@ -38,6 +39,17 @@ def estrategia():
     ts.registerCallback(callbackEstrategia)
 
     rospy.spin()
+
+def sala3():
+    pubGarra = rospy.Publisher('garra', Int8MultiArray, queue_size=10)
+    rate = rospy.Rate(20)
+
+    dataGarra = Int8MultiArray()
+
+    dataGarra = [0,0] # braco, mão
+
+    pubGarra.publish(dataGarra)
+
 
 if __name__ == "__main__":
 	try:
