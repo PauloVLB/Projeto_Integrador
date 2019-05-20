073d65b26f0f70bf3b7bb549c25816db4281cdea
Isaac Marlon da Silva Lourenço <isaacmarlondasilvalourenco@gmail.com>
2019-05-20 19:17:16 -0300

    Revert "Renomeação de pacote teste_video"
    This reverts commit c72db174b1d4b29525d2159139c1402c57db40f2.

--- Testes/Testes Paulinho/teste_video/scripts/estrategia.py
+++ Testes/Testes Paulinho/teste_video/scripts/estrategia.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 import rospy
 import message_filters
 from std_msgs.msg import Float64MultiArray
 from std_msgs.msg import Int32MultiArray
-from std_msgs.msg import Int8MultiArray # 1byte -128 to 127, utilizamos de 0 a 90
-from testevideo.msg import SensoresDistanciaMsg, RefletanciaMsg
+from teste_video.msg import SensoresDistanciaMsg, RefletanciaMsg
+
+
 
 def callbackEstrategia(refle, dist):
-    pubMotores = rospy.Publisher('motores', Int32MultiArray, queue_size=10
+    pubMotores = rospy.Publisher('motores', Int32MultiArray, queue_size=10)
     rate = rospy.Rate(20)
 
     maisEsq = refle.refletancia[0]
@@ -17,7 +17,6 @@ def callbackEstrategia(refle, dist):
     dir = refle.refletancia[2]
     maisDir = refle.refletancia[3]
     dataMotores = Int32MultiArray()
-
     if(esq > 4 and dir > 4 ) :
         dataMotores.data = [25, 25]
     elif (esq > 4 and dir < 4  ):
@@ -39,17 +38,6 @@ def estrategia():
     ts.registerCallback(callbackEstrategia)
 
     rospy.spin()
-
-def sala3():
-    pubGarra = rospy.Publisher('garra', Int8MultiArray, queue_size=10)
-    rate = rospy.Rate(20)
-
-    dataGarra = Int8MultiArray()
-
-    dataGarra = [0,0] # braco, mão
-
-    pubGarra.publish(dataGarra)
-
 
 if __name__ == "__main__":
 	try:
