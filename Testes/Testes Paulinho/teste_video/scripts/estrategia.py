aaf68c07a23ffad5cd187318299fc0fffa81507c
Isaac Marlon da Silva Lourenço <isaacmarlondasilvalourenco@gmail.com>
2019-05-31 13:49:51 -0300

    Estrategia transformada em classe

--- Testes/Testes Paulinho/teste_video/scripts/estrategia.py
+++ Testes/Testes Paulinho/teste_video/scripts/estrategia.py
@@ -10,109 +10,90 @@ from teste_video.msg import SensoresDistanciaMsg, RefletanciaMsg, Posicao
 import time
 
 # publishers
-pubMotores = rospy.Publisher('motores', Int32MultiArray, queue_size=10)
-pubGarras = rospy.Publisher('garra', Int32MultiArray, queue_size=10)
 
+class Estrategia():
+    def __init__(self):
 
-def callbackEstrategia(refle, dist, posicao):
+        self.pubMotores = rospy.Publisher('motores', Int32MultiArray, queue_size=10)
+        self.pubGarras = rospy.Publisher('garra', Int32MultiArray, queue_size=10)
+        self.posicaoRobo = 1 # 1 == SALA 1 E 2 // 2 == RAMPA // 3 == SALA 3
 
-    rate = rospy.Rate(20)
+    def callbackEstrategia(refle, dist):
 
-    # setando
-    maisEsq = refle.refletancia[0]
-    esq = refle.refletancia[1]
-    dir = refle.refletancia[2]
-    maisDir = refle.refletancia[3]
+        rate = rospy.Rate(20)
 
-    # datas
-    dataMotores = Int32MultiArray()
-    dataGarras = Int32MultiArray()
+        # setando
+        refleMaisEsq = refle.refletancia[0]
+        refleEsq = refle.refletancia[1]
+        refleDir = refle.refletancia[2]
+        refleMaisDir = refle.refletancia[3]
 
-    if posicao.sala == 1: ''' sala 1 e 2 '''
-        # seguir linha
-        if(esq > 4 and dir > 4 ): # branco, branco
-            dataMotores.data = [25, 25]
-        elif (esq > 4 and dir < 4  ): # branco, preto
+        sonarFrontal = dist.sensoresDistancia[0]
+        sonarDir = dist.sensoresDistancia[1]
+        sonarEsq = dist.sensoresDistancia[2]
+
+        # datas
+        dataMotores = Int32MultiArray()
+        dataGarras = Int32MultiArray()
+
+        # garra
+        self.garras = Garras(pubGarras)
+
+        if self.posicaoRobo == 1: ''' sala 1 e 2 '''
+            # seguir linha
+            if(esq > 4 and dir > 4 ): # branco, branco
+                dataMotores.data = [25, 25]
+            elif (esq > 4 and dir < 4  ): # branco, preto
+                dataMotores.data = [25, -25]
+            elif (esq < 4 and dir > 4 ): # preto, branco
+                dataMotores.data = [-25,25]
+            elif (esq < 4 and dir < 4 ): # preto, preto
+                dataMotores.data = [25, 25]
+
+        elif self.posicaoRobo == 2: ''' rampa '''
+            # subir rampa
+            if(esq > 4 and dir > 4): # branco, branco
+                dataMotores.data = [25, 25] # ?
+            elif (esq > 4 and dir < 4  ): # branco, preto
+                dataMotores.data = [25, -25] # ?
+            elif (esq < 4 and dir > 4 ): # preto, branco
+                dataMotores.data = [-25,25] # ?
+            elif (esq < 4 and dir < 4 ): # preto, preto
+                dataMotores.data = [25, 25] # ?
+                #self.posicao = 3
+
+        elif self.posicao == 3: ''' sala 3 '''
+
+            # girar para alinhar
             dataMotores.data = [25, -25]
-        elif (esq < 4 and dir > 4 ): # preto, branco
-            dataMotores.data = [-25,25]
-        elif (esq < 4 and dir < 4 ): # preto, preto
+            pubMotoresDelay(100)
+            # encostar na parede
+            dataMotores.data = [-25, -25]
+            pubMotoresDelay(200)
+            # ir para frente
             dataMotores.data = [25, 25]
+            pubMotoresDelay(200)
+            # encostar na parede
+            dataMotores.data = [-25, -25]
+            pubMotoresDelay(200)
+
+            ''' TESTE GARRA '''
+            #garras.abaixarBraco()
 
 
-    elif posicao.sala == 2: ''' rampa '''
-        # subir rampa
-        if(esq > 4 and dir > 4): # branco, branco
-            dataMotores.data = [25, 25] # ?
-        elif (esq > 4 and dir < 4  ): # branco, preto
-            dataMotores.data = [25, -25] # ?
-        elif (esq < 4 and dir > 4 ): # preto, branco
-            dataMotores.data = [-25,25] # ?
-        elif (esq < 4 and dir < 4 ): # preto, preto
-            dataMotores.data = [25, 25] # ?
+    def estrategia():
+        rospy.init_node('estrategia', anonymous=True)
+        subRefle = message_filters.Subscriber('refletancia', RefletanciaMsg)
+        subDistancia = message_filters.Subscriber('distancia', SensoresDistanciaMsg)
+        subPosicao = message_filters.Subscriber('posicao', Posicao)
 
+        ts = message_filters.TimeSynchronizer([subRefle, subDistancia, subPosicao], 10)
+        ts.registerCallback(callbackEstrategia)
 
-    elif posicao.sala == 3: ''' sala 3 '''
-
-        # girar para alinhar
-        dataMotores.data = [25, -25]
-        pubMotoresDelay(100)
-        # encostar na parede
-        dataMotores.data = [-25, -25]
-        pubMotoresDelay(200)
-        # ir para frente
-        dataMotores.data = [25, 25]
-        pubMotoresDelay(200)
-        # encostar na parede
-        dataMotores.data = [-25, -25]
-        pubMotoresDelay(200)
-
-        ''' TESTE GARRA '''
-        time = Time()
-        time.sleep(300)
-
-        # if robolog.achouVitima():
-
-        dataAngInicial = garras.getAbaixarInicial()
-        dataAngFinal = garras.getAbaixarFinal()
-
-        # [90, 80] to [10, 10]?
-
-        pubGarras.publish(dataGarras)
-
-    #pubMotores.publish(dataMotores)
-    #pubGarras.publish(dataGarras)
-
-def pubMotoresDelay(delay):
-    time = Time()
-    i = 0
-    while not i < 2:
-        pubMotores.publish(dataMotores)
-        time.sleep(delay)
-        i = i + 1
-
-def pubGarraDelay(delay):
-    time = Time()
-    i = 0
-    while not i < 2:
-        pubGarras.publish(dataGarras)
-        time.sleep(delay)
-        i = i + 1
-
-
-def estrategia():
-    rospy.init_node('estrategia', anonymous=True)
-    subRefle = message_filters.Subscriber('refletancia', RefletanciaMsg)
-    subDistancia = message_filters.Subscriber('distancia', SensoresDistanciaMsg)
-    subPosicao = message_filters.Subscriber('posicao', Posicao)
-
-    ts = message_filters.TimeSynchronizer([subRefle, subDistancia, subPosicao], 10)
-    ts.registerCallback(callbackEstrategia)
-
-    rospy.spin()
+        rospy.spin()
 
 if __name__ == "__main__":
 	try:
-	      estrategia()
+	      self.estrategia()
 	except rospy.ROSInterruptException:
 		pass
